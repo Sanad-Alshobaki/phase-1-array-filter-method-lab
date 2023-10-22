f4af27c7670827db316c4f323c3f5b5c05a5e415
Sanad-Alshobaki <alshobakisanad@gmail.com>
2023-10-23 01:41:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, name) {
+    return arr.filter((element) => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(arr, fLetter) {
+    return arr.filter((element) => element.toLowerCase().indexOf(fLetter.toLowerCase()) === 0)
+}
+
+function matchName(arr, name) {
+    return arr.filter((element) => element.name === name);
+}
